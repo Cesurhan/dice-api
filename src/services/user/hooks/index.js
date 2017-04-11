240c999f2db1f84313c1bda6dfde92825c4e2892
Cesurhan <ces-ur@hotmail.com>
2017-04-11 17:32:28 +0200

    Minor fix

--- src/services/user/hooks/index.js
+++ src/services/user/hooks/index.js
@@ -16,7 +16,6 @@ exports.before = {
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
   ],
   create: [
     auth.hashPassword(),
